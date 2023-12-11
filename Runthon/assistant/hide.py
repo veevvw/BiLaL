0e91ea8f9902990e2456807b41d9e237e874042a
veevvw <149306702+veevvw@users.noreply.github.com>
2023-12-11 18:12:06 +0300

    Update hide.py

--- Runthon/assistant/hide.py
+++ Runthon/assistant/hide.py
@@ -4,18 +4,18 @@ import re
 
 from telethon.events import CallbackQuery
 
-from Tepthon import zedub
+from Runthon import zedub
 
 
 @zedub.tgbot.on(CallbackQuery(data=re.compile(b"hide_(.*)")))
 async def on_plug_in_callback_query_handler(event):
     timestamp = int(event.pattern_match.group(1).decode("UTF-8"))
-    if os.path.exists("./Tepthon/hide.txt"):
-        jsondata = json.load(open("./Tepthon/hide.txt"))
+    if os.path.exists("./Runthon/hide.txt"):
+        jsondata = json.load(open("./Runthon/hide.txt"))
         try:
             reply_pop_up_alert = jsondata[f"{timestamp}"]["text"]
         except KeyError:
-            reply_pop_up_alert = "- عذرًا .. هذه الرسـالة لم تعد موجـوده في سيـرفرات تـيـبثون"
+            reply_pop_up_alert = "- عذرًا .. هذه الرسـالة لم تعد موجـوده في سيـرفرات رنثون"
     else:
-        reply_pop_up_alert = "- عذرًا .. هذه الرسـالة لم تعد موجـوده في سيـرفرات تــيـبثون"
+        reply_pop_up_alert = "- عذرًا .. هذه الرسـالة لم تعد موجـوده في سيـرفرات رنثون"
     await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
