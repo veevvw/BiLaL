f7fe2a6128d1b4a1b8603c14ad81d48868cc8d13
veevvw <149306702+veevvw@users.noreply.github.com>
2023-12-12 13:00:38 +0300

    Update اكينوتر.py

--- Runthon/plugin/اكينوتر.py
+++ Runthon/plugin/اكينوتر.py
@@ -11,7 +11,7 @@ from telethon import Button
 from telethon.errors import BotMethodInvalidError
 from telethon.events import CallbackQuery, InlineQuery
 
-from Tepthon import zedub
+from Runthon import zedub
 
 from ..Config import Config
 from ..core.decorators import check_owner
