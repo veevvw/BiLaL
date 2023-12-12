f5e1f2b5c1c68b9f9382725def709ee41bc1a29a
veevvw <149306702+veevvw@users.noreply.github.com>
2023-12-12 12:50:31 +0300

    Update converter.py

--- Runthon/helpers/functions/converter.py
+++ Runthon/helpers/functions/converter.py
@@ -2,11 +2,11 @@ import os
 
 from PIL import Image
 
-from Tepthon.core.logger import logging
-from Tepthon.core.managers import edit_or_reply
-from Tepthon.helpers.functions.vidtools import take_screen_shot
-from Tepthon.helpers.tools import fileinfo, media_type, meme_type
-from Tepthon.helpers.utils.utils import runcmd
+from Runthon.core.logger import logging
+from Runthon.core.managers import edit_or_reply
+from Runthon.helpers.functions.vidtools import take_screen_shot
+from Runthon.helpers.tools import fileinfo, media_type, meme_type
+from Runthon.helpers.utils.utils import runcmd
 
 LOGS = logging.getLogger(__name__)
 
