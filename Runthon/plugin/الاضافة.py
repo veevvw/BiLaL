9fdffe7840c53b26c5a1e7093fe9c84558bc77fb
veevvw <149306702+veevvw@users.noreply.github.com>
2023-12-12 13:01:52 +0300

    Update الاضافة.py

--- Runthon/plugin/الاضافة.py
+++ Runthon/plugin/الاضافة.py
@@ -2,7 +2,7 @@ from telethon import functions
 from telethon.tl import functions
 from telethon.tl.functions.channels import InviteToChannelRequest
 
-from Tepthon import zedub
+from Runthon import zedub
 
 from ..core.managers import edit_delete, edit_or_reply
 
